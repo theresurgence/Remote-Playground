b9d680f19dae965457a5f2389db5f62a220ef220
Kureans <leonardtengys@gmail.com>
2021-05-27 18:48:16 +0800

    updated images, added live chat feature & login mock-up

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000;
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
 
@@ -42,12 +42,17 @@ io.on('connection', (socket) => { //when a new client connects to server, websoc
         io.sockets.emit('gpio3_click', gpio3_status);
     });
 
+    socket.on('message', (message) => {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0,2)}: ${message}`);
+    });
+
 
 });
 
 
 
-/************************************ COMMENT OUT if not PI  **********************************/
+/************************************ COMMENT OUT if not PI  **********************************
 const toggle = require('./gpio-toggle').toggle; //import toggle fn from gpio-toggle module
 const videoStream = require('raspberrypi-node-camera-web-streamer/videoStream');
 videoStream.acceptConnections(app, {
@@ -56,7 +61,7 @@ videoStream.acceptConnections(app, {
     fps: 16,
     encoding: 'JPEG',
     quality: 10 //lower is faster
-}, '/stream.mjpg', true);
+}, '/stream.mjpg', true); */
 
 
 app.use(express.static(__dirname+'/public')); //front-end files in public
