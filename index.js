972ecaca23e707288c8bc8c4aaeb6b8277cc241b
Kureans <leonardtengys@gmail.com>
2021-06-06 15:59:59 +0800

    integrated login auth feature (not complete)

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
 const sqlite3 = require('sqlite3');
 const path = require('path');
 // const gpio = require('./gpio-toggle'); //import gpio functions and variables
-const socket = require('socket.io');       //import socket server                           
+const socket = require('socket.io');       //import socket server    
+const passport = require('passport');
+const bcrypt = require('bcrypt');    
+const flash = require('express-flash');
+const session = require('express-session');   
+
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+const users = []; //eventually store users' data in database
 
 /************************************ COMMENT OUT if not PI  **********************************/
 // const videoStream = require('raspberrypi-node-camera-web-streamer/videoStream');
@@ -35,7 +52,6 @@ var gpio1_status, gpio2_status, gpio3_status, gpio4_status= 0;
 var online = 0; //number of online users
 
 var simon_on = false; 
-
 
 io.on('connection', (socket) => { //when a new client connects to server, websocket connected!
     console.log(socket.id, 'connected');
@@ -102,7 +118,48 @@ io.on('connection', (socket) => { //when a new client connects to server, websoc
 
 
 app.use(express.static(__dirname+'/public')); //front-end files in public
+app.use(express.urlencoded({ extended: false}));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/public/index.html');
+})
+
+app.get('/signup', (req, res) => {
+    res.sendFile(__dirname+'/public/signup.html');
+});
+
+app.get('/profile', (req, res) => {
+    res.sendFile(__dirname+'/public/profile.html');
+});
+
+app.post('/profile', passport.authenticate('local', { successRedirect: '/profile', failureRedirect: '/', failureFlash: true }));
+
+
+app.post('/signup', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        res.redirect('/');
+    } catch {
+        res.redirect('/signup');
+    }
+    console.log(users);
+});
 
 
 
